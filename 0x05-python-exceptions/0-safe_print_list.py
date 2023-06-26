d4670ec46a402a1fb12e6744110155a9b990b78f
Nasreddine Braik <breakeinfo0@gmail.com>
2023-06-26 01:44:10 -0700

    updated files

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    """Print x elememts of a list.
+
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+
+    Returns:
+        The number of elements printed.
+    """
+    ret = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return (ret)
+
+
+=============================
+1-safe_print_integer.py
+
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+    """Print an integer with "{:d}".format().
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        If a TypeError or ValueError occurs - False.
+        Otherwise - True.
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)`
